55696211af80a457fe39fbda37666d245992e9c8
ViktorMakTorwend <vikmactor@gmail.com>
2024-10-11 18:49:18 +0300

    Add var

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
-﻿
-Console.WriteLine("Hello");
-Console.Write("Vik\n");
-Console.ReadLine();
-Console.ReadKey();
-// comment 
+﻿// For console
+
+// Console.WriteLine("Hello");
+// Console.Write("Vik\n");
+// Console.ReadLine();
+// Console.ReadKey();
+
+// Data types
+
+// byte number_min = 255;
+// short number_mid = 32000;
+// int number_big = 2000000000;
+// long number_very_big = 2000000000000000000;
+// float number_float = 1000.517652f;
+// double number_double = 5.2354d;
+
+// string word = "Hello";
+// char symbol = 'a';
+
+// bool isHappy = true;
+
+// Console.WriteLine(number_min);
+// Console.WriteLine(number_mid);
+// Console.WriteLine(number_big);
+// Console.WriteLine(number_very_big);
+// Console.WriteLine(number_float);
+// Console.WriteLine(number_double);
+// Console.WriteLine(word);
+// Console.WriteLine(symbol);
+// Console.WriteLine(isHappy);
+
+// int num_1 = 0, num_2 = 0;
+// num_1 = Convert.ToInt32(Console.ReadLine());
+// num_2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("First: " + num_1 + " " + "Second: " + num_2);
+
