22274e571d1dbd5c2c62dd2058dc9088c3be6f71
ViktorMakTorwend <vikmactor@gmail.com>
2024-10-14 11:30:06 +0300

    Add cycle

--- Program.cs
+++ Program.cs
@@ -1,58 +1,28 @@
-﻿// int a = 5;
-// if (a == 5)
-// {
-//     Console.WriteLine("Number is five");
+﻿// for(byte i = 0; i < 10; i++) {
+//     Console.WriteLine("Element: {0}", i);
 // }
 
-// bool word = true;
-// if (word)
-// {
-//     Console.WriteLine("Word is true");
+// byte k = 0;
+// while(k < 10) {
+//    Console.WriteLine("Element: {0}", k);
+//    k++;
 // }
 
-// if (a > 6)
-// {
-//     Console.WriteLine("A is greter");
-// }
-// else
-// {
-//     Console.WriteLine("A is less");
-// }
-
-// if (a > 4 && word)
-// {
-//     Console.WriteLine("Both are right");
+// bool isHasCar = true;
+// while(isHasCar) {
+//     Console.WriteLine("Input the word");
+//     string end = Console.ReadLine();
+//     if(end =="end") {
+//         isHasCar = false;
+//     }
 // }
 
-// if (a > 9 || word)
-// {
-//     Console.WriteLine("At least one is right");
-// }
-
-Console.Write("Enter role: ");
-string role = Console.ReadLine();
-if (role == "admin")
-{
-    Console.Write("Enter your name: ");
-    string user_name = Console.ReadLine();
-    Console.Write("Enter {0} age: ", user_name);
-    short age = Convert.ToInt16(Console.ReadLine());
-
-    if (age <= 0 || age > 99)
-    {
-        Console.Write("Enter {0} age: ", user_name);
-        age = Convert.ToInt16(Console.ReadLine());
+for (short i = 0; i < 10; i++) {
+    // if (i > 5) {
+    //     break;
+    // }
+    if(i % 2 == 0) {
+        continue;
     }
-
-    if (age <= 0 || age > 99)
-    {
-        Console.WriteLine("Error");
-        age = 0;
-    } else {
-        Console.WriteLine("User's age is: " + age);
-    }
-}
-else
-{
-    Console.WriteLine("No permits");
+    Console.WriteLine("Element: {0}", i);
 }
